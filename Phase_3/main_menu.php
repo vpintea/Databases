86a4d4726598c34e1a23e7a06c2b67e8a03b6116
Valentin Pintea <vpintea3@gatech.edu>
2021-04-10 20:26:07 -0700

    created report 6 and updated main menu

--- Phase_3/main_menu.php
+++ Phase_3/main_menu.php
@@ -5,6 +5,7 @@ include ('lib/header.php'); ?>
 <ul>
     <a href="report_4.php"><strong>Report 4</strong></a> <br>
     <a href="report_5.php"><strong>Report 5</strong></a> <br>
+    <a href="report_6.php"><strong>Report 6</strong></a> <br>
     <a href="report_9.php"><strong>Report 9</strong></a> 
 </ul>
 
