2339dead2867ff11e81f116b895e64042759ef80
Valentin Pintea <vpintea3@gatech.edu>
2021-04-11 12:37:45 -0700

    updated main menu format

--- Phase_3/main_menu.php
+++ Phase_3/main_menu.php
@@ -3,13 +3,27 @@ include ('lib/header.php'); ?>
 
 <h1>Main Menu</h1>
 
+<form method="post">
+  
+  <input type="Submit" name="Submit" value="Submit">
+</form>
+
+<br>
+
+
+
 <h2>Reports</h2>
 <ul>
-    <a href="report_4.php"><strong>Report 4</strong></a> <br>
-    <a href="report_5.php"><strong>Report 5</strong></a> <br>
-    <a href="report_6.php"><strong>Report 6</strong></a> <br>
-    <a href="report_7.php"><strong>Report 7</strong></a> <br>
-    <a href="report_9.php"><strong>Report 9</strong></a> 
+    <a href="report_4.php"><strong>View Outdoor Furniture on Groundhog Day Report (Report 4)</strong></a> <br>
+    <br>
+    <a href="report_5.php"><strong>State with Highest Volume for each Category Report (Report 5)</strong></a> <br>
+    <br>
+    <a href="report_6.php"><strong>View Revenue by Population Report (Report 6)</strong></a> <br>
+    <br>
+    <a href="report_7.php"><strong>Childcare Sales Volume Report (Report 7)</strong></a> <br>
+    <br>
+    <a href="report_9.php"><strong>Advertising Campaign Analysis Report (Report 9)</strong></a> 
+    
 </ul>
 
 <?php include ("lib/footer.php"); ?>
